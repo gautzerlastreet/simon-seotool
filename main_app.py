71af0056dedf9689bed4c29815e2fd0439741a60
Psimon8 <121551167+Psimon8@users.noreply.github.com>
2024-11-30 01:02:36 +0000

    V3

--- main_app.py
+++ main_app.py
@@ -1,19 +1,20 @@
 import streamlit as st
 from apps.occurus_rewrite import occurus_rewrite
+from apps.similarity_refine import similarity_refine
+
 
 # Titre principal de l'application
-st.sidebar.title("Navigation")
+st.sidebar.title("More tools, More Fun.")
 
 # Menu latéral pour choisir une page
 page = st.sidebar.selectbox(
-    "Choisissez une page",
-    ["Occurus Rewrite"]
+    "Choisissez une application",
+    ["Occurus Rewrite"],
+    ["Similarity Refine"]
 )
 
 # Logique pour charger la page sélectionnée
-if page == "Occurus Rewrite":
+if page == "Similarity Refine":
+    apps.similarity_refine()
+elif page == "Occurus Rewrite":
     apps.occurus_rewrite.app()
-elif page == "Rankings Google Maps":
-    google_maps.app()
-elif page == "Bulk Semantic Analyzer":
-    semantic_analyzer.app()

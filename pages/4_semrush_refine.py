c19a905054392a8ce38243ad16f2b594f2098e9c
Psimon8 <121551167+Psimon8@users.noreply.github.com>
2024-12-01 23:16:37 +0000

    mmp

--- pages/4_semrush_refine.py
+++ pages/4_semrush_refine.py
@@ -109,3 +109,18 @@ if uploaded_file:
         )
     else:
         st.error("The uploaded file must contain 'Keyword', 'Position', and 'Search Volume' columns.")
+
+
+        # Ajouter l'icône Twitter et le texte en bas à gauche
+st.sidebar.markdown(
+    """
+    <div style="position: fixed; bottom: 10px; left: 20px;">
+        <a href="https://twitter.com/lekoz_simon" target="_blank" style="text-decoration: none;">
+            <img src="https://abs.twimg.com/favicons/twitter.3.ico" 
+                 alt="Twitter" style="width:20px; vertical-align: middle; margin-right: 5px;">
+            <span style="color: white; font-size: 14px;">@lekoz_simon</span>
+        </a>
+    </div>
+    """,
+    unsafe_allow_html=True
+)

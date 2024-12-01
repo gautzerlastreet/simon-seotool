791ea9b2788087c25a3ce8fe7ebbb86ccb54bb15
Psimon8 <121551167+Psimon8@users.noreply.github.com>
2024-12-01 13:26:18 +0000

    V2

--- Home.py
+++ Home.py
@@ -1,6 +1,4 @@
 import streamlit as st
-my_var = "This a variable from Home.py"
-
 
 # Configuration de la page Streamlit
 st.set_page_config(
@@ -12,6 +10,6 @@ st.set_page_config(
 
 def main():
     st.header("SEO Tools Box")
-    
+
 if __name__ == '__main__':
     main()

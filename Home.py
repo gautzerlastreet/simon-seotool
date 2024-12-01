0684169570e150f2e3a61b06bd3faad2c81812bc
Psimon8 <121551167+Psimon8@users.noreply.github.com>
2024-12-01 12:23:45 +0000

    z

--- Home.py
+++ Home.py
@@ -16,14 +16,7 @@ def main():
     st.write(my_var)
    
     choix = st.sidebar.radio("Navigation", ["Home", "Similarity Refine", "PAA Extractor"])
-    if choix == "Home":
-        st.subheader("subheader")
-    elif choix == "Similarity Refine":
-        st.subheader("subheader SR")
-    elif choix == "PAA Extractor":
-        st.subheader("subheader PA")
-    else:
-        st.subheader("Please select an option from the sidebar")
+
         
 
 if __name__ == '__main__':

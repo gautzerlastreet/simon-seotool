c8064032566e35f19614d1732009347f492c97c9
Psimon8 <121551167+Psimon8@users.noreply.github.com>
2024-12-01 09:42:56 +0000

    L1 sucess

--- Home.py
+++ Home.py
@@ -1,6 +1,15 @@
 import streamlit as st
 my_var = "This a variable from Home.py"
 
+
+# Configuration de la page Streamlit
+st.set_page_config(
+    layout="wide",
+    page_title="SEO Tools Box",
+    page_icon="📌"
+)
+
+
 def main():
     st.header("SEO Tools Box")
     st.title("Simon's Tools Box")
